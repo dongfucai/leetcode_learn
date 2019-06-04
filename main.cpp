cb6949436db10be2103fddc0450d7310115971a4
dongfucai <1766318593@qq.com>
2019-06-04 23:27:22 +0800

    151-medium

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ medium_151_reverse_string.cpp
@@ -0,0 +1,67 @@
+//
+// Created by 董付财 on 2019/5/30.
+//151. Reverse Words in a String
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    void reverse(string &s, int start, int end) {
+        while(start < end) {
+            swap(s[start], s[end]);
+            start++;
+            end--;
+        }
+    }
+    string reverseWords(string s) {
+
+        int size = s.size() ;
+        if (size == 0) {
+            return "";
+        }
+        reverse(s, 0, size - 1);
+
+        int start = 0;
+        string res;
+        for(int i = 0; i < size; ++i) {
+            if(s[i] == ' ') {
+                reverse(s, start, i-1);
+                start = i + 1;
+            }
+            if(i == (s.size() -1 )) {
+                reverse(s, start, i);
+            }
+        }
+
+        int x = 0;
+        int y = size - 1;
+        while(s[x] == ' ') {
+            x++;
+        }
+        while(s[y] == ' ') {
+            y--;
+        }
+        for(int i = x ; i <= y; ++i) {
+            res.push_back(s[i]);
+            while(s[i]==' ' && (i + 1) <= y && s[i+1] == ' ') {
+                i++;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
